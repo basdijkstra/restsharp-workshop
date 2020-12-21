a17f8b02224c19ef73289a029eacce24c9e5b387
Bas Dijkstra <bas@ontestautomation.com>
2020-12-21 17:01:01 +0100

    Initial commit

---
+++ RestSharpWorkshop/Examples/Examples1.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using System.Linq;
+using System.Net;
+
+namespace RestSharpWorkshop.Examples
+{
+    [TestFixture]
+    public class Examples1
+    {
+        // The base URL for our example tests
+        private const string BASE_URL = "http://jsonplaceholder.typicode.com";
+
+        // The RestSharp client we'll use to make our requests
+        private RestClient client;
+
+        [OneTimeSetUp]
+        public void SetupRestSharpClient()
+        {
+            client = new RestClient(BASE_URL);
+        }
+
+        [Test]
+        public void GetDataForUser1_CheckStatusCode_ShouldBeHttpOK()
+        {
+            RestRequest request = new RestRequest("/users/1", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        [Test]
+        public void GetDataForUser1_CheckStatusCode_ShouldBeHttp200()
+        {
+            RestRequest request = new RestRequest("/users/1", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void GetDataForUser2_CheckContentType_ShouldBeApplicationJson()
+        {
+            RestRequest request = new RestRequest("/users/2", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.That(response.ContentType, Does.Contain("application/json"));
+        }
+
+        [Test]
+        public void GetDataForUser3_CheckServerHeader_ShouldBeCloudflare()
+        {
+            RestRequest request = new RestRequest("/users/3", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            string serverHeaderValue = response.Headers
+                .Where(x => x.Name.Equals("Server"))
+                .Select(x => x.Value.ToString())
+                .FirstOrDefault();
+
+            Assert.That(serverHeaderValue, Is.EqualTo("cloudflare"));
+        }
+
+        [Test]
+        public void GetDataForUser4_CheckName_ShouldBePatriciaLebsack()
+        {
+            RestRequest request = new RestRequest("/users/4", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            var responseData = JObject.Parse(response.Content);
+
+            Assert.That(responseData.GetValue("name").ToString(), Is.EqualTo("Patricia Lebsack"));
+        }
+    }
+}
