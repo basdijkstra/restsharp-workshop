e927217f16c4ed9e5f8ea43f4721792305d6091f
Bas Dijkstra <bas@ontestautomation.com>
2022-02-16 15:01:21 +0100

    Add models to use in deserialization example

---
+++ RestSharpWorkshop/Examples/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace RestSharpWorkshop.Examples.Models
+{
+    public class Company
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("catchPhrase")]
+        public string CatchPhrase { get; set; }
+        [JsonProperty("bs")]
+        public string BS { get; set; }
+    }
+}
---
+++ RestSharpWorkshop/Examples/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace RestSharpWorkshop.Examples.Models
+{
+    public class User
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("username")]
+        public string Username { get; set; }
+        [JsonProperty("company")]
+        public Company Company { get; set; }
+    }
+}
