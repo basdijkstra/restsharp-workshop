549b0c42a966982976acbab8fa3fff4116c264a3
Bas Dijkstra <bas@ontestautomation.com>
2025-01-31 09:00:24 +0100

    WIP: add client model example

---
+++ RestSharpWorkshop/ClientModelExample/ClientModelTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using RestSharpWorkshop.ClientModelExample.Client;
+using RestSharpWorkshop.ClientModelExample.Models;
+using System;
+using System.Net;
+
+namespace RestSharpWorkshop.ClientModelExample
+{
+    [TestFixture]
+    public class ClientModelTest
+    {
+        private Uri baseUri = new Uri("https://jsonplaceholder.typicode.com");
+
+        private PostClient postClient;
+
+        [SetUp]
+        public void CreateRestClient()
+        {
+            postClient = new PostClient(baseUri);
+        }
+
+        [Test]
+        public void TestClientModel()
+        {
+            Post post = new Post
+            {
+                UserId = 1,
+                Title = "My new blog post",
+                Body = "This is awesome",
+            };
+
+            var response = postClient.CreatePost(post);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+        }
+
+        [Test]
+        public void TestClientModelWithGet()
+        {
+            var response = postClient.GetPost("1");
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+    }
+}
---
+++ RestSharpWorkshop/ClientModelExample/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace RestSharpWorkshop.ClientModelExample.Models
+{
+    public class Post
+    {
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+    }
+}
